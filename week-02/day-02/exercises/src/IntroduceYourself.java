45d720e71d4e07cc920904e57eb1228b607b595d
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 12:29:17 +0100

    Add exercise-04 solution

--- week-02/day-02/exercises/src/IntroduceYourself.java
+++ week-02/day-02/exercises/src/IntroduceYourself.java
@@ -1,17 +1,10 @@
 public class IntroduceYourself {
   public static void main(String[] args) {
-    // Write a program that prints a few details to the terminal window about you
-    // It should print each detail to a new line.
-    //  - Your name
-    //  - Your age
-    //  - Your height in meters (as a decimal fraction)
-    //
-    //  Example output:
-    //  John Doe
-    //  31
-    //  1.87
-
-
-
+    //  - My name:
+    System.out.println("Marcell Mészáros");
+    //  - My age:
+    System.out.println(33);
+    //  - My height in meters (as a decimal fraction):
+    System.out.println(1.91);
   }
 }
