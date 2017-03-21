28d477c23dee987b40a7c402f46a506ac5c83b20
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 17:25:30 +0100

    Add exercise-16 solution

--- week-02/day-02/exercises/src/AnimalsAndLegs.java
+++ week-02/day-02/exercises/src/AnimalsAndLegs.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class AnimalsAndLegs {
 
   public static void main(String[] args) {
@@ -5,5 +7,14 @@ public class AnimalsAndLegs {
     // The first represents the number of chickens the farmer has
     // The seconf represents the number of pigs the farmer has
     // It should print how many legs all the animals have
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("How many chickens does the farmer have?");
+    int userInAmountChickens = scanner.nextInt();
+    System.out.println("And how many pigs does the farmer have?");
+    int userInAmountPigs = scanner.nextInt();
+    System.out.println("All the animals have "
+            + (userInAmountChickens * 2
+            + userInAmountPigs * 4)
+            + " legs!");
   }
 }
