a126260f6dc8bbce56e663171ebd632c1c6450a0
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 16:20:36 +0100

    Add exercise-12 original source

---
+++ week-02/day-02/exercises/src/Swap.java
@@ -0,0 +1,11 @@
+public class Swap {
+
+  public static void main(String[] args) {
+    int a = 123;
+    int b = 526;
+    // Swap the values of the wariables
+
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
