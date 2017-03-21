64ca11802b9f47aafd122a364d9aba67808c1b92
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 12:54:39 +0100

    Add exercise-05 solution

--- week-02/day-02/exercises/src/TwoNumbers.java
+++ week-02/day-02/exercises/src/TwoNumbers.java
@@ -1,18 +1,10 @@
 public class TwoNumbers {
   public static void main(String[] args) {
-    // Create a program that prints a few operations on two numbers: 22 and 13
-
-    // Print the result of 13 added to 22
-
-    // Print the result of 13 substracted from 22
-
-    // Print the result of 22 mutiplied by 13
-
-    // Print the result of 22 divided by 13 (as a decimal fraction)
-
-    // Print the integer part of 22 divided by 13
-
-    // Print the reminder of 22 divided by 13
-
+    System.out.println(22 + 13); // 13 added to 22
+    System.out.println(22 - 13); // 13 substracted from 22
+    System.out.println(22 * 13); // 22 mutiplied by 13
+    System.out.println(22 / 13.); // 22 divided by 13 (as decimal fraction)
+    System.out.println(22 / 13); // integer part of 22 divided by 13
+    System.out.println(22 % 13); // remainder of 22 divided by 13
   }
 }
