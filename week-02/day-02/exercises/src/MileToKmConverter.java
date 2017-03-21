39855b3c3247e4f5cbbdc466468236b5be7ed681
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 17:16:54 +0100

    Add exercise-15 solution

--- week-02/day-02/exercises/src/MileToKmConverter.java
+++ week-02/day-02/exercises/src/MileToKmConverter.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class MileToKmConverter {
 
   public static void main(String[] args) {
     // Write a program that asks for an integer that is a distance in kilometers,
     // then it converts that value to miles and prints it
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Convert kilometers to miles!\n");
+    System.out.println("Enter kilometer value (whole number): ");
+    int userInputKilometers = scanner.nextInt();
+    float milesConvertedFromKilometers = (float) ((int) (userInputKilometers * 16.0934)) / 10;
+    System.out.println(userInputKilometers + " km = " + milesConvertedFromKilometers + " mi");
   }
 }
