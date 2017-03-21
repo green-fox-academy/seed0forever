98470656930724de9af042b3465e6cf09eec5c0f
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 16:51:19 +0100

    Add exercise-13 solution

--- week-02/day-02/exercises/src/VariableMutation.java
+++ week-02/day-02/exercises/src/VariableMutation.java
@@ -1,68 +1,69 @@
 public class VariableMutation {
-  
+
   public static void main(String[] args) {
     int a = 3;
-    // make it bigger by 10
+    // 01) make it bigger by 10
+    a += 10;
 
-
-    System.out.println(a);
+    System.out.println("01) " + a);
 
 
 
 
     int b = 100;
-    // make it smaller by 7
+    // 02) make it smaller by 7
+    b -= 7;
 
-
-    System.out.println(b);
+    System.out.println("02) " + b);
 
 
 
 
     int c = 44;
-    // please double c's value
+    // 03) please double c's value
+    c *= 2;
 
-
-    System.out.println(c);
+    System.out.println("03) " + c);
 
 
 
 
     int d = 125;
-    // please divide by 5 d's value
+    // 04) please divide by 5 d's value
+    d /= 5;
 
-
-    System.out.println(d);
+    System.out.println("04) " + d);
 
 
 
 
     int e = 8;
-    // please cube of e's value
+    // 05) please cube of e's value
+    e *= e * e;
 
-
-    System.out.println(e);
+    System.out.println("05) " + e);
 
 
 
 
     int f1 = 123;
     int f2 = 345;
-    // tell if f1 is bigger than f2 (print as a boolean)
-
+    // 06) tell if f1 is bigger than f2 (print as a boolean)
+    System.out.println("06) " + (f1 > f2));
 
 
 
 
     int g1 = 350;
     int g2 = 200;
-    // tell if the double of g2 is bigger than g1 (print as a boolean)
+    // 07) tell if the double of g2 is bigger than g1 (print as a boolean)
+    System.out.println("07) " + (2 * g2 > g1));
 
 
 
-
-    int h = 1357988018575474;
-    // tell if it has 11 as a divisor (print as a boolean)
+    long h = 1357988018575474L;
+    // 08) tell if it has 11 as a divisor (print as a boolean)
+    System.out.println("07) " + (h % 11 == 0));
 
 
 
@@ -70,21 +71,23 @@ public class VariableMutation {
 
     int i1 = 10;
     int i2 = 3;
-    // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
-
+    // 08) tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+    System.out.println("08) " + (i1 > i2 * i2 && i1 < i2 * i2 * i2));
 
 
 
     int j = 1521;
-    // tell if j is dividable by 3 or 5 (print as a boolean)
-
+    // 09) tell if j is dividable by 3 or 5 (print as a boolean)
+    System.out.println("09) " + (j % 3 == 0 || j % 5 == 0));
 
 
 
     String k = "Apple";
-    //fill the k variable with its cotnent 4 times
+    // 10) fill the k variable with its content 4 times
+    k += k;
+    k += k;
 
 
-    System.out.println(k);
+    System.out.println("10) " + k);
   }
 }
