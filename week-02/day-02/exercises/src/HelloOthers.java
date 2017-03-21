78f0d2a5620c242ac558bbb6249a8a9e7e8a62d2
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 12:08:36 +0100

    Add exercise-02 solution

--- week-02/day-02/exercises/src/HelloOthers.java
+++ week-02/day-02/exercises/src/HelloOthers.java
@@ -1,10 +1,7 @@
 public class HelloOthers {
     public static void main(String[] args) {
-        // Greet 3 of your classmates with this program, in three separate lines
-        // like:
-        //
-        // Hello, Esther!
-        // Hello, Mary!
-        // Hello, Joe!
+        System.out.println("Hello, Norbi! :)");
+        System.out.println("Hello, Ramin! :)");
+        System.out.println("Hello, Viktor! :)");
     }
 }
