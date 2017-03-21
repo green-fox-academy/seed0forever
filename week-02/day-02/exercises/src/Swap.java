555f7fdaf4882a48aa7cbcf671afd2e7bd1b6878
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 16:26:59 +0100

    Add exercise-12 solution

--- week-02/day-02/exercises/src/Swap.java
+++ week-02/day-02/exercises/src/Swap.java
@@ -3,8 +3,11 @@ public class Swap {
   public static void main(String[] args) {
     int a = 123;
     int b = 526;
-    // Swap the values of the wariables
-
+    // Swap the values of the variables
+    int aPlusB = a + b;
+    a = aPlusB - a;
+    b = aPlusB - b;
+    // Print the new value of the variables
     System.out.println(a);
     System.out.println(b);
   }
