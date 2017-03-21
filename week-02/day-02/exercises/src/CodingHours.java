3608acbd84665829d65efac3fd943adde9939fc9
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 14:03:02 +0100

    Add exercise-06 solution

--- week-02/day-02/exercises/src/CodingHours.java
+++ week-02/day-02/exercises/src/CodingHours.java
@@ -2,11 +2,11 @@ public class CodingHours {
   public static void main(String[] args) {
     // An average Green Fox attendee codes 6 hours daily
     // The semester is 17 weeks long
-    //
-    // Print how many hours is spent with coding in a semester by an attendee,
-    // if the attendee only codes on workdays.
-    //
-    // Print the percentage of the coding hours in the semester if the average
-    // work hours weekly is 52
+
+    // Amount of hours spent with coding during a semester by an attendee,
+    // if the attendee only codes on workdays:
+    System.out.println("An average Green Fox attendee spends " + (6 * 5 * 17) + " hours with coding in a semester.\n");
+
+    System.out.println("On average, attendees spend " + (int) ((6 * 5 * 17.) / (52 * 17) * 100) + "% of an 52-hour work week on coding.");
   }
 }
