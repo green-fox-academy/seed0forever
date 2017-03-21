c12b2f7e48f01d79c1d24a639eca74fdf242e807
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 15:34:19 +0100

    Add exercise-08 solution

--- week-02/day-02/exercises/src/DefineBasicInfo.java
+++ week-02/day-02/exercises/src/DefineBasicInfo.java
@@ -1,10 +1,17 @@
 public class DefineBasicInfo {
 
   public static void main(String[] args) {
-    // Define several things as a variable than print their values
-    // Your name as a string
-    // Your age as an integer
-    // Your height in meters as a double
-    // Wether you are married or not as a boolean
+    // My name as a string
+    String myName = "Marcell Mészáros";
+    System.out.println("My name is: " + myName);
+    // My age as an integer
+    int myAge = 33;
+    System.out.println("My age is: " + myAge + " years");
+    // My height in meters as a double
+    double myHeightInMeters = 1.91;
+    System.out.println("My height is: " + myHeightInMeters + " meters");
+    // Whether I am married or not as a boolean
+    boolean amIMarried = false;
+    System.out.println("Married: " + amIMarried);
   }
 }
