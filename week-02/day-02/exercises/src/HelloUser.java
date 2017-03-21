9d8c865922cfb7492101e95d2faa6801c6dea457
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-21 16:58:20 +0100

    Add exercise-14 solution

--- week-02/day-02/exercises/src/HelloUser.java
+++ week-02/day-02/exercises/src/HelloUser.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class HelloUser {
 
   public static void main(String[] args) {
     // Modify this program to greet user instead of the World!
     // The program should ask for the name of the user
-    System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter your name:");
+    String userInputName = scanner.nextLine();
+    System.out.println("Hello, " + userInputName + "!");
   }
 }
