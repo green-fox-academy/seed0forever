49ae37adcd4aa656cd6d3dba60a9cc8f947c491a
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-22 20:06:25 +0100

    Add w02d02 exercise-26 solution

---
+++ week-02/day-02/exercises/src/MultiplicationTable.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+  public static void main(String[] args) {
+    // Create a program that asks for a number and prints the multiplication table with that number
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Multiplication Table");
+    System.out.println("Show multiplication table for this number:");
+    int userInBaseNumber = scanner.nextInt();
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i + " * " + userInBaseNumber + " = " + i * userInBaseNumber);
+    }
+    // Example:
+    // The number 15 should print:
+    //
+    // 1 * 15 = 15
+    // 2 * 15 = 30
+    // 3 * 15 = 45
+    // 4 * 15 = 60
+    // 5 * 15 = 75
+    // 6 * 15 = 90
+    // 7 * 15 = 105
+    // 8 * 15 = 120
+    // 9 * 15 = 135
+    // 10 * 15 = 150
+  }
+
+}
