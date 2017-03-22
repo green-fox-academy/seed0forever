d2878542f9feb7ee029d35a218995a0b4f6f00ed
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-22 16:53:43 +0100

    Add exercise-11 solution

---
+++ week-02/day-03/exercises/src/IncrementElement.java
@@ -0,0 +1,13 @@
+public class IncrementElement {
+
+  public static void main(String[] args) {
+//    - Create an array variable named `t`
+//    with the following content: `[1, 2, 3, 4, 5]`
+    int[] t = {1, 2, 3, 4, 5};
+//    - Increment the third element
+    t[2]++;
+    System.out.println(t[2]);
+//            - Print the third element
+  }
+
+}
