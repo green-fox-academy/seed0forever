8133c42d06505a6a47367d860eff65d14586b417
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-22 16:54:12 +0100

    Add exercise-12 solution

---
+++ week-02/day-03/exercises/src/PrintAll.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class PrintAll {
+
+  public static void main(String[] args) {
+    // - Create an array variable named `af`
+//   with the following content: `[4, 5, 6, 7]`
+    int[] af = {4, 5, 6, 7};
+// - Print all the elements of `af`
+    System.out.println(Arrays.toString(af));
+
+  }
+
+}
