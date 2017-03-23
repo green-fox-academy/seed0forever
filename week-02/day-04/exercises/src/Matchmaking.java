99aa0cc40a4f4fd739f0fd83ad438080e7ce3930
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-23 21:51:41 +0100

    Add w02d04 Matchmaking solution

---
+++ week-02/day-04/exercises/src/Matchmaking.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Matchmaking {
+
+  public static void main(String... args) {
+    ArrayList<String> girls = new ArrayList<String>(
+            Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+    ArrayList<String> boys = new ArrayList<String>(
+            Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
+    ArrayList<String> order = new ArrayList<String>();
+
+    // Join the two lists by matching one girl with one boy in the order list
+    // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+    int numOfPairs = girls.size() > boys.size() ? boys.size() : girls.size();
+    for (int i = 0; i < numOfPairs; i++) {
+      order.add(girls.get(i));
+      order.add(boys.get(i));
+    }
+    System.out.println(order);
+  }
+}
