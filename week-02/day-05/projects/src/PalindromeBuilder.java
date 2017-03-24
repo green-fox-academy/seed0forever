4483671a644bbe9b439097c9197b61695d1afca5
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-24 14:34:46 +0100

    Fix w02d05 PalindromeBuilder output string creation

--- week-02/day-05/projects/src/PalindromeBuilder.java
+++ week-02/day-05/projects/src/PalindromeBuilder.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 public class PalindromeBuilder {
 
   public static void main(String[] args) {
+    String reversedString = "";
+    String palindromeString = "";
     Scanner scanner = new Scanner(System.in);
     System.out.println("Palindrome Builder");
     System.out.println("Enter a string to build a palindrome from:");
     String userInString = scanner.nextLine();
+/*
     String[] originalStringCharacters = userInString.split("");
     String reversedString = "";
     for (int i = originalStringCharacters.length - 1; i >= 0; i--) {
       reversedString += originalStringCharacters[i];
     }
-    System.out.println(reversedString);
+*/
+    for (int i = userInString.length() - 1; i >= 0; i--) {
+      reversedString += userInString.charAt(i);
+    }
+    palindromeString = userInString + reversedString;
+    System.out.println(palindromeString);
   }
 
 }
