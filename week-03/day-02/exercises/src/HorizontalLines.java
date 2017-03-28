36c0d298d77146be1e3a5d5bebd5cce9a3762207
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-28 11:33:50 +0200

    Add w03d02 ex05 HorizontalLines solution

---
+++ week-03/day-02/exercises/src/HorizontalLines.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines {
+
+  public static void mainDraw(Graphics graphics) {
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a 50 long horizontal line from that point.
+    // draw 3 lines with that function.
+    drawLineToCenter(5, 15, graphics);
+    drawLineToCenter(180, 80, graphics);
+    drawLineToCenter(250, 270, graphics);
+  }
+
+  private static void drawLineToCenter(int x, int y, Graphics graphics) {
+    graphics.setColor(new Color(0, 0, 0, 255));
+    graphics.drawLine(x, y, x + 50, y);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
