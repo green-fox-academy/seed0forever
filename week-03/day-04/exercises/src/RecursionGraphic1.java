97024f4406a17f1260cb0b50209a241e45c4a467
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-03-31 11:13:20 +0200

    Finish w03d04 ex11 RecursionGraphic1 solution

--- week-03/day-04/exercises/src/RecursionGraphic1.java
+++ week-03/day-04/exercises/src/RecursionGraphic1.java
@@ -7,24 +7,51 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class RecursionGraphic1 {
 
-  static int WIDTH = 601;
-  static int HEIGHT = 601;
+  static int WIDTH = 600;
+  static int HEIGHT = 600;
 
   public static void mainDraw(Graphics graphics) {
     final int SQUARE_SPLIT = 3;
-    int squareSide = (WIDTH > HEIGHT) ? HEIGHT - 1 : WIDTH - 1;
 
-    drawSquaresRecursive(graphics, squareSide, SQUARE_SPLIT);
+    final int LAYERS = 5;
+    int squareSide = (WIDTH > HEIGHT) ? HEIGHT : WIDTH;
+    int squareTopLeftX = 0;
+    int squareTopLeftY = 0;
+    graphics.setColor(new Color(0, 0, 0, 200));
+    graphics.drawRect(squareTopLeftX, squareTopLeftY, squareSide - 1, squareSide - 1);
 
-
+    drawRecursiveSquares(graphics, squareTopLeftX, squareTopLeftY, squareSide, SQUARE_SPLIT,
+            LAYERS);
   }
 
-  private static void drawSquaresRecursive(Graphics graphics, int squareSide, int squareSplit) {
+  private static void drawRecursiveSquares(Graphics graphics, int topLeftX,
+          int topLeftY, int side, int split, int layers) {
+    drawSquaresSplitters(graphics, topLeftX, topLeftY, side, split);
+    if (layers <= 1) {
+      return;
+    }
+    --layers;
+    drawRecursiveSquares(graphics, topLeftX + side / split, topLeftY, side / split, split,
+            layers);
+    drawRecursiveSquares(graphics, topLeftX, topLeftY + side / split, side / split, split,
+            layers);
+    drawRecursiveSquares(graphics, topLeftX + side / split * 2, topLeftY + side / split,
+            side / split, split,
+            layers);
+    drawRecursiveSquares(graphics, topLeftX + side / split, topLeftY + side / split * 2,
+            side / split, split,
+            layers);
+  }
+
+  private static void drawSquaresSplitters(Graphics graphics, int topLeftX, int topLeftY,
+          int side, int split) {
     graphics.setColor(new Color(0, 0, 0, 200));
-    graphics.drawLine(squareSide / squareSplit, 0, squareSide / squareSplit, squareSide);
-    graphics.drawLine(squareSide / squareSplit * 2, 0, squareSide / squareSplit * 2, squareSide);
-    graphics.drawLine(0, squareSide / squareSplit, squareSide, squareSide / squareSplit);
-    graphics.drawLine(0, squareSide / squareSplit * 2, squareSide, squareSide / squareSplit * 2);
+    graphics.drawLine(topLeftX + side / split, topLeftY, topLeftX + side / split, topLeftY + side);
+    graphics.drawLine(topLeftX + side / split * 2, topLeftY, topLeftX + side / split * 2,
+            topLeftY + side);
+    graphics.drawLine(topLeftX, topLeftY + side / split, topLeftX + side, topLeftY + side / split);
+    graphics.drawLine(topLeftX, topLeftY + side / split * 2, topLeftX + side,
+            topLeftY + side / split * 2);
   }
 
 
