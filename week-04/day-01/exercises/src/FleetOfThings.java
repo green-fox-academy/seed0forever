3212961723a43a64532bc4bff851f1018db95bda
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-03 14:41:37 +0200

    Add w04d01 ex05 FleetOfThings exercise solution

--- week-04/day-01/exercises/src/FleetOfThings.java
+++ week-04/day-01/exercises/src/FleetOfThings.java
@@ -8,6 +8,19 @@ public class FleetOfThings {
     // 3. [x] Stand up
     // 4. [x] Eat lunch
 
+    fleet.add(new Thing("Get milk"));
+    fleet.add(new Thing("Remove the obstacles"));
+    fleet.add(newThingWithStatus("Stand up", true));
+    fleet.add(newThingWithStatus("Remove the obstacles", true));
+
     System.out.println(fleet);
+  }
+
+  private static Thing newThingWithStatus(String thingName, boolean completed) {
+    Thing newThing = new Thing(thingName);
+    if (completed == true) {
+      newThing.complete();
+    }
+    return newThing;
   }
 }
