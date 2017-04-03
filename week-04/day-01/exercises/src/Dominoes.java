6b207e02c2b1bde0fa17080a59a50bd4738b9283
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-03 12:34:57 +0200

    Add w04d01 ex03 Dominoes first solution

--- week-04/day-01/exercises/src/Dominoes.java
+++ week-04/day-01/exercises/src/Dominoes.java
@@ -6,10 +6,34 @@ public class Dominoes {
   public static void main(String[] args) {
     List<Domino> dominoes = initializeDominoes();
     // You have the list of Dominoes
-    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // Order them into one snake where the adjacent dominoes have the same numbers
+    // on their adjacent sides
     // eg: [2, 4], [4, 3], [3, 5] ...
 
+    List<Domino> dominoesSorted = sortDominoes(dominoes);
+
     System.out.println(dominoes);
+    System.out.println(dominoesSorted);
+  }
+
+  public static List<Domino> sortDominoes(List<Domino> dominoList) {
+    List<Domino> dominoesSorted = new ArrayList<>();
+
+    dominoesSorted.add(dominoList.get(0));
+    dominoList.remove(0);
+
+    while (dominoList.size() > 0) {
+      for (int i = 0; i < dominoList.size(); i++) {
+        if (dominoesSorted.get(dominoesSorted.size() - 1).getValues()[1] == dominoList.get(i)
+                .getValues()[0]) {
+          dominoesSorted.add(dominoList.get(i));
+          dominoList.remove(i);
+          continue;
+        }
+      }
+    }
+    return dominoesSorted;
+
   }
 
   static List<Domino> initializeDominoes() {
