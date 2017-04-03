ac87fe8f386ad9bde4042ff5708c24eddceae393
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-03 16:56:22 +0200

    Refactor w04d01 ex07 Sharpie to separate class definition and main()

--- week-04/day-01/exercises/src/Sharpie.java
+++ week-04/day-01/exercises/src/Sharpie.java
@@ -18,15 +18,4 @@ public class Sharpie {
     this.inkAmount--;
   }
 
-  public static void main(String[] args) {
-    Sharpie testSharpie = new Sharpie("green", 20);
-    System.out.println("Created testSharpie.");
-    System.out.println("testSharpie color: " + testSharpie.color + ", width: " + testSharpie.width
-            + ", inkAmount: " + testSharpie.inkAmount);
-    testSharpie.use();
-    System.out.println("Used testSharpie once.");
-    System.out.println("testSharpie color: " + testSharpie.color + ", width: " + testSharpie.width
-            + ", inkAmount: " + testSharpie.inkAmount);
-  }
-
 }
---
+++ week-04/day-01/exercises/src/SharpieApp.java
@@ -0,0 +1,15 @@
+public class SharpieApp {
+
+  public static void main(String[] args) {
+    Sharpie testSharpie = new Sharpie("green", 20);
+    System.out.println("Created testSharpie.");
+    System.out.println("testSharpie color: " + testSharpie.color + ", width: " + testSharpie.width
+            + ", inkAmount: " + testSharpie.inkAmount);
+    testSharpie.use();
+    System.out.println("Used testSharpie once.");
+    System.out.println("testSharpie color: " + testSharpie.color + ", width: " + testSharpie.width
+            + ", inkAmount: " + testSharpie.inkAmount);
+  }
+
+
+}
