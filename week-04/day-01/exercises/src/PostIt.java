d7ae2e3a679d69b80a7356728856d0aefd7f31ab
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-03 11:07:39 +0200

    Add w04d01 ex01 PostIt class and create 3 instances

---
+++ week-04/day-01/exercises/src/PostIt.java
@@ -0,0 +1,23 @@
+public class PostIt {
+  String backgroundColor;
+  String text;
+  String textColor;
+
+  public static void main(String[] args) {
+    PostIt note1 = new PostIt();
+    note1.backgroundColor = "orange";
+    note1.textColor = "blue";
+    note1.text = "Idea 1";
+
+    PostIt note2 = new PostIt();
+    note2.backgroundColor = "pink";
+    note2.textColor = "black";
+    note2.text = "Awesome";
+
+    PostIt note3 = new PostIt();
+    note3.backgroundColor = "yellow";
+    note3.textColor = "green";
+    note3.text = "Superb!";
+  }
+
+}
