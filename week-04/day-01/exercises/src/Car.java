861763e2516f0639f54609f96942fb302a0e430b
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-03 16:20:32 +0200

    Add w04d01 ex10 petrol-station exercise first solution

---
+++ week-04/day-01/exercises/src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+  float gasAmount, capacity;
+
+  public Car() {
+    this(0f, 100f);
+  }
+
+  public Car(float gasAmount, float capacity) {
+    this.gasAmount = gasAmount;
+    this.capacity = capacity;
+  }
+
+}
---
+++ week-04/day-01/exercises/src/Station.java
@@ -0,0 +1,15 @@
+public class Station {
+
+  float gasAmount;
+
+  public void refill(Car car) {
+    if (gasAmount >= car.capacity - car.gasAmount) {
+      gasAmount -= car.capacity - car.gasAmount;
+      car.gasAmount += car.capacity - car.gasAmount;
+    } else {
+      car.gasAmount += gasAmount;
+      gasAmount = 0f;
+    }
+  }
+
+}
