d3d16645ce2b409f7415d5c8d9f114fe45b99f94
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-05 15:59:35 +0200

    w04d03 ex05 rename method in Fibonacci class

--- week-04/day-03/exercises/src/Fibonacci.java
+++ week-04/day-03/exercises/src/Fibonacci.java
@@ -1,12 +1,12 @@
 public class Fibonacci {
 
-  public static int fibonacci(int n) {
+  public static int getNthIndexValue(int n) {
     if (n == 1) {
       return 1;
     } else if (n == 0) {
       return 0;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return getNthIndexValue(n - 1) + getNthIndexValue(n - 2);
   }
 
 }
