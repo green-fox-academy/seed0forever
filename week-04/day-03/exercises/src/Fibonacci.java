c38fa8d832f1dd1351fedb57c9fb746d403fd815
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-05 15:10:58 +0200

    w04d03 ex05 created Fibonacci class from w03d04 ex10

---
+++ week-04/day-03/exercises/src/Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+
+  public static int fibonacci(int n) {
+    if (n == 1) {
+      return 1;
+    } else if (n == 0) {
+      return 0;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+}
