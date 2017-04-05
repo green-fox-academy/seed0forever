0c3f4a5978a0d1364fca82a4129b35768d80f3e9
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-05 19:24:48 +0200

    w04d03 ex06 fix Extension translate() method

--- week-04/day-03/exercises/src/Extension.java
+++ week-04/day-03/exercises/src/Extension.java
@@ -5,6 +5,7 @@ import java.util.List;
  * Created by aze on 2017.04.04..
  */
 public class Extension {
+
   List<Character> vowelList;
 
   public Extension() {
@@ -30,14 +31,14 @@ public class Extension {
   }
 
   String translate(String hungarian) {
-    String teve = hungarian;
-    int length = teve.length();
+    String teve = "";
+    int length = hungarian.length();
     for (int i = 0; i < length; i++) {
-      char c = teve.charAt(i);
+      char c = hungarian.charAt(i);
       if (isVowel(c)) {
-        teve = String.join(c + "v" + c, teve.split("" + c));
-        i += 2;
-        length += 2;
+        teve += c + "v" + c;
+      } else {
+        teve += hungarian.charAt(i);
       }
     }
     return teve;
