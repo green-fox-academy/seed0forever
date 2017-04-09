8e6f43b502d2a95f45cb3784165695c4c1060040
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-09 11:20:46 +0200

    w03d02 ex15 LinePlay refactor drawing from main() to a new function

--- week-03/day-02/exercises/src/LinePlay.java
+++ week-03/day-02/exercises/src/LinePlay.java
@@ -12,6 +12,10 @@ public class LinePlay {
     int canvasWidth = 300; // Don't know yet how to get these from main
     int canvasHeight = 300; // Don't know yet how to get these from main
 
+    drawLinePlay(canvasWidth, canvasHeight, graphics);
+  }
+
+  private static void drawLinePlay(int canvasWidth, int canvasHeight, Graphics graphics) {
     for (int i = 0; i < canvasWidth; i += 20) {
       graphics.setColor(new Color(176, 78, 240, 255));
       graphics.drawLine(i, 0, canvasWidth, i);
