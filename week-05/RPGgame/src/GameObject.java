dfbb452c96370120fe066621efa8cffbd2710df0
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-10 18:09:08 +0200

    w05 RPGgame create GameObject class, similar to PositionedImage class

---
+++ week-05/RPGgame/src/GameObject.java
@@ -0,0 +1,41 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GameObject {
+
+  BufferedImage image;
+  int posColumn;
+  int posRow;
+  int size;
+
+  public GameObject(String filename, int size, int posColumn, int posRow) {
+    this.posColumn = posColumn;
+    this.posRow = posRow;
+    this.size = size;
+    setImage(filename);
+  }
+
+  protected void setImage(String filename) {
+    try {
+      image = ImageIO.read(new File(filename));
+      System.out.println("Success reading image file '" + filename + "'.");
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Failed to read image file '" + filename + "'");
+    }
+  }
+
+  public void draw(Graphics graphics){
+    if (image != null) {
+      graphics.drawImage(image, posColumn * size, posRow * size, null);
+      System.out.println("Executed " + this.getClass().getSimpleName() + "'s draw method.");
+    }
+  }
+
+  public BufferedImage getImage() {
+    return image;
+  }
+}
