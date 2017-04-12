210f6f9353032348251cc9ad41f1a31025041771
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-12 15:02:21 +0200

    w05 RPGgame rename Map to GameMap

--- week-05/RPGgame/src/GameMap.java
+++ week-05/RPGgame/src/GameMap.java
@@ -1,7 +1,7 @@
-public class Map {
+public class GameMap {
 
+  // In the MAP array, 0 = floor; 1 = wall
   public static final int[][] MAP = {
-          // 0 = floor; 1 = wall
           {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
           {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
           {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
