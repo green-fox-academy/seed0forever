fbecf99061aa25ef687256943da004d30abf3a29
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-12 10:40:00 +0200

    w05 RPGgame add ImageLoader singleton container

---
+++ week-05/RPGgame/src/ImageLoader.java
@@ -0,0 +1,34 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+
+  private static ImageLoader instance;
+
+  public BufferedImage FLOOR, WALL, HERO_DOWN, HERO_UP, HERO_RIGHT, HERO_LEFT, BOSS;
+
+  private ImageLoader() {
+    try {
+      FLOOR = ImageIO.read(new File("assets/floor.png"));
+      WALL = ImageIO.read(new File("assets/wall.png"));
+      HERO_DOWN = ImageIO.read(new File("assets/hero-down.png"));
+      HERO_UP = ImageIO.read(new File("assets/hero-up.png"));
+      HERO_RIGHT = ImageIO.read(new File("assets/hero-right.png"));
+      HERO_LEFT = ImageIO.read(new File("assets/hero-left.png"));
+      BOSS = ImageIO.read(new File("assets/boss.png"));
+      System.out.println("Success reading image files.");
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Failed to read image files.");
+    }
+  }
+
+  public static ImageLoader getInstance() {
+    if (instance == null) {
+      instance = new ImageLoader();
+    }
+    return instance;
+  }
+}
