6239b31b7a5442b49817c8780e644f8ddba38340
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-14 22:19:16 +0200

    w04d02 'Aircraft Carrier' add Main app and create fleets

--- week-04/day-02/exercises/src/aircraftcarrier/Main.java
+++ week-04/day-02/exercises/src/aircraftcarrier/Main.java
@@ -3,6 +3,24 @@ package aircraftcarrier;
 public class Main {
 
   public static void main(String[] args) {
-    F16 firstF16 = new F16();
+    AircraftCarrier firstCarrier = new AircraftCarrier(1000);
+    firstCarrier.addAircraft("F16");
+    firstCarrier.addAircraft("F16");
+    firstCarrier.addAircraft("F35");
+    firstCarrier.addAircraft("F35");
+    firstCarrier.addAircraft("F35");
+    firstCarrier.addAircraft("F35");
+    firstCarrier.fill();
+    System.out.println(firstCarrier.getStatus());
+
+    AircraftCarrier secondCarrier = new AircraftCarrier(4000);
+    secondCarrier.addAircraft("F16");
+    secondCarrier.addAircraft("F16");
+    secondCarrier.addAircraft("F16");
+    secondCarrier.addAircraft("F16");
+    secondCarrier.addAircraft("F35");
+    secondCarrier.addAircraft("F35");
+    secondCarrier.fill();
+    System.out.println(secondCarrier.getStatus());
   }
 }
