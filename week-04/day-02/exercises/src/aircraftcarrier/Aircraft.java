7f40c3ce10b8ea1d91746cf8d3b66e93a5e8dbab
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-14 17:58:27 +0200

    w04d02 'Aircraft Carrier' add fight method

--- week-04/day-02/exercises/src/aircraftcarrier/Aircraft.java
+++ week-04/day-02/exercises/src/aircraftcarrier/Aircraft.java
@@ -9,4 +9,10 @@ public class Aircraft {
   public Aircraft() {
     currentAmmo = 0;
   }
+
+  int fight() {
+    int causedDamage = currentAmmo * baseDamage;
+    currentAmmo = 0;
+    return causedDamage;
+  }
 }
