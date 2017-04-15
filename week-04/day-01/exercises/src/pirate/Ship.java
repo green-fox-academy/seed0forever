25ed34ea3e15890228e9a561d0847095783529f9
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-15 17:02:52 +0200

    w04d01 'Pirates' Ship: add fillShip()

--- week-04/day-01/exercises/src/pirate/Ship.java
+++ week-04/day-01/exercises/src/pirate/Ship.java
@@ -10,7 +10,17 @@ public class Ship {
 
   Ship() {
     crew = new ArrayList<>();
+  }
+
+  void fillShip() {
     captain = new Captain();
+    int numberOfPirates = 50 + (int) (Math.random() * 51);
+
+    for (int i = 1; i <= numberOfPirates; i++) {
+      Pirate pirate = new Pirate();
+      pirate.setIdNumber(i);
+      crew.add(new Pirate());
+    }
   }
 
 }
