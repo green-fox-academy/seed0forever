cfa20b6de3e132c401d76ceb27a4380396e11312
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-15 14:47:49 +0200

    w04d01 'Pirates' add fields, drinkSomeRum(), howsItGoingMate()

--- week-04/day-01/exercises/src/pirate/Pirate.java
+++ week-04/day-01/exercises/src/pirate/Pirate.java
@@ -1,5 +1,25 @@
 package pirate;
 
-public class Pirate {
+class Pirate {
+  private int rumsDrank;
+  private boolean isDead;
+
+  Pirate() {
+    rumsDrank = 0;
+    isDead = false;
+  }
+
+  void drinkSomeRum() {
+    rumsDrank++;
+  }
+
+  void howsItGoingMate() {
+    String askMoreRum = "Pour me anudder!";
+    String tooDrunk = "Arghh, I'ma Pirate. How d'ya d'ink its goin?";
+
+    String answer = (rumsDrank < 5) ? askMoreRum : tooDrunk;
+    System.out.println(answer);
+  }
+
 
 }
