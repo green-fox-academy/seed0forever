2e8418bb927e2e3f3b6bcedad35b89c6072993dd
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-15 15:43:12 +0200

    w04d01 'Pirates' add Ship fields and constructor

--- week-04/day-01/exercises/src/pirate/Ship.java
+++ week-04/day-01/exercises/src/pirate/Ship.java
@@ -1,5 +1,16 @@
 package pirate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ship {
+
+  private List<Pirate> crew;
+  private Captain captain;
+
+  Ship() {
+    crew = new ArrayList<>();
+    captain = new Captain();
+  }
 
 }
