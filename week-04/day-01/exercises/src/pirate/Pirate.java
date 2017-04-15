31782b0036f806f0b644e047b580850801ea012b
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-15 15:35:35 +0200

    w04d01 'Pirates' implement Pirate brawl() and passOut()

--- week-04/day-01/exercises/src/pirate/Pirate.java
+++ week-04/day-01/exercises/src/pirate/Pirate.java
@@ -4,12 +4,16 @@ class Pirate {
 
   private int rumsDrank;
   private boolean isAlive;
+  private boolean isPassedOut;
   private String deadMessage;
+  private String anotherDeadMessage;
 
   Pirate() {
     rumsDrank = 0;
     isAlive = true;
+    isPassedOut = false;
     deadMessage = "He's dead!";
+    anotherDeadMessage = "The other pirate is dead!";
   }
 
   void drinkSomeRum() {
@@ -33,7 +37,36 @@ class Pirate {
   }
 
   void brawl(Pirate anotherPirate) {
+    if (!isAlive) {
+      System.out.println(deadMessage);
+    } else if (!anotherPirate.isAlive) {
+      System.out.println(anotherDeadMessage);
+    } else {
+      // outcome: what is the result of the fight
+      // 1 - this Pirate dies
+      // 2 - the other Pirate dies
+      // 3 - both Pirates pass out
+      int outcome = 1 + (int) (Math.random() * 3);
+      switch (outcome) {
+        case 1:
+          die();
+          break;
+        case 2:
+          anotherPirate.die();
+          break;
+        case 3:
+          passOut();
+          anotherPirate.passOut();
+        default:
+      }
 
+    }
+
+
+  }
+
+  private void passOut() {
+    isPassedOut = true;
   }
 
 
