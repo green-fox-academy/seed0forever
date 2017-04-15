6a6ae88e54c5d2c4273ec0d8c1c597d15d8f207a
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-15 17:02:08 +0200

    w04d01 'Pirates' add ShipTest

---
+++ week-04/day-01/exercises/tests/pirate/ShipTest.java
@@ -0,0 +1,21 @@
+package pirate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShipTest {
+
+  @Test
+  void construct() {
+    Ship ship = new Ship();
+
+  }
+
+
+  @Test
+  void fillShip() {
+
+  }
+
+}
