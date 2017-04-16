6c574ad5a8fa2d4a77d5a6d8a8299e3f049040f8
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-16 19:56:08 +0200

    w05 RPGgame add Skeleton class

---
+++ week-05/RPGgame/src/Skeleton.java
@@ -0,0 +1,10 @@
+import java.awt.image.BufferedImage;
+
+public class Skeleton extends Monster {
+
+  public Skeleton(int size, int posColumn, int posRow, BufferedImage imageFaceDown,
+          BufferedImage imageFaceUp, BufferedImage imageFaceLeft,
+          BufferedImage imageFaceRight) {
+    super(size, posColumn, posRow, imageFaceDown, imageFaceUp, imageFaceLeft, imageFaceRight);
+  }
+}
