7fb48ee4522b1d6dd2b0013cd32b18b7bb45836f
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-18 15:44:54 +0200

    w06d01 ex01 'Instruments' add abstract classes

---
+++ week-06/day-01/workshop/src/main/java/music/StringedInstrument.java
@@ -0,0 +1,9 @@
+package main.java.music;
+
+public abstract class StringedInstrument extends Instrument {
+
+  int numberOfStrings;
+  final String formatForPlay = "%s, a %d-stringed instrument that %s\n";
+
+
+}
