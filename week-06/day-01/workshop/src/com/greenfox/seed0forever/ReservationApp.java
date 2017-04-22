425d5ae8e6121de617b2306eefd216271c867d81
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-22 14:43:35 +0200

    w06d01 ex03 'Reservations' add generateCodeAlphabet

--- week-06/day-01/workshop/src/com/greenfox/seed0forever/ReservationApp.java
+++ week-06/day-01/workshop/src/com/greenfox/seed0forever/ReservationApp.java
@@ -1,9 +1,13 @@
 package com.greenfox.seed0forever;
 
+import com.greenfox.seed0forever.reservations.*;
+
 public class ReservationApp {
 
   public static void main(String[] args) {
 
+    for (int i = 0; i < Reservation.getCodeAlphabet().length; i++) {
+      System.out.printf(Reservation.getCodeAlphabet()[i]);
+    }
   }
-
 }
--- week-06/day-01/workshop/src/com/greenfox/seed0forever/reservations/Reservation.java
+++ week-06/day-01/workshop/src/com/greenfox/seed0forever/reservations/Reservation.java
@@ -2,6 +2,25 @@ package com.greenfox.seed0forever.reservations;
 
 public class Reservation implements Reservationy {
 
+  private static final String[] codeAlphabet = generateCodeAlphabet();
+
+  private static String[] generateCodeAlphabet() {
+    String[] codeAlphabet = new String[10 + (91 - 65)];
+
+    for (int arrayIndex = 0; arrayIndex < codeAlphabet.length; arrayIndex++) {
+      if (arrayIndex < 10) {
+        codeAlphabet[arrayIndex] = String.valueOf(arrayIndex);
+      } else {
+        codeAlphabet[arrayIndex] = Character.toString((char) ('A' + arrayIndex - 10));
+      }
+    }
+    return codeAlphabet;
+  }
+
+  public static String[] getCodeAlphabet() {
+    return codeAlphabet;
+  }
+
   @Override
   public String getDowBooking() {
     return null;
