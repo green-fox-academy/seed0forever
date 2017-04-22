667c3b6e3f537df4a1ae9460a6c3cd3f7377b8ea
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-22 13:46:34 +0200

    w06d01 ex02 'CharSequence' implement solution

--- week-06/day-01/workshop/src/charoperations/AppCharOperations.java
+++ week-06/day-01/workshop/src/charoperations/AppCharOperations.java
@@ -4,6 +4,11 @@ public class AppCharOperations {
 
   public static void main(String[] args) {
     String testSequence1 = "12345 abcdef";
+    CharOperations testCharOperations = new CharOperations(testSequence1);
 
+    System.out.println(testCharOperations.toString());
+    System.out.println(testCharOperations.length());
+    System.out.println(testCharOperations.charAt(3));
+    System.out.println(testCharOperations.subSequence(3, 9));
   }
 }
--- week-06/day-01/workshop/src/charoperations/CharOperations.java
+++ week-06/day-01/workshop/src/charoperations/CharOperations.java
@@ -2,23 +2,35 @@ package charoperations;
 
 public class CharOperations implements CharSequence {
 
+  private String input;
+
+  public CharOperations(String input) {
+    this.input = input;
+  }
+
   @Override
   public String toString() {
-    return super.toString();
+    return reverse(input);
+  }
+
+  private String reverse(String input) {
+    StringBuilder original = new StringBuilder(input);
+    return original.reverse().toString();
   }
 
   @Override
   public int length() {
-    return 0;
+    return input.length();
   }
 
   @Override
   public char charAt(int index) {
-    return 0;
+    return input.charAt(input.length() - index - 1);
   }
 
   @Override
   public CharSequence subSequence(int start, int end) {
-    return null;
+    String reversed = reverse(input);
+    return reversed.substring(start, end);
   }
 }
