5898577241812b2c5429620d1a2f07fab14af988
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-26 11:51:10 +0200

    todoext-team initial commit

---
+++ week-06/day-04/todoextended-rek-mar-nor-pat/src/main/java/FileHandler.java
@@ -0,0 +1,21 @@
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandler {
+
+  public static List<String[]> readFile(String filePathName) {
+    List<String[]> lines = new ArrayList<>();
+
+    try {
+      FileReader fileReader = new FileReader(filePathName);
+      CSVReader reader = new CSVReader(fileReader, ';');
+      lines = reader.readAll();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return lines;
+  }
+}
