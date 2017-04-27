c6689bdf2f9a172c5beff9f9a9bed465a0b206e0
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-27 17:20:23 +0200

    todoext-team remove StringUtils dependency

--- week-06/day-04/todoextended-rek-mar-nor-pat/src/main/java/entity/TodoFactory.java
+++ week-06/day-04/todoextended-rek-mar-nor-pat/src/main/java/entity/TodoFactory.java
@@ -1,17 +1,18 @@
 package entity;
 
 import java.time.ZonedDateTime;
-import org.apache.commons.lang3.StringUtils;
 
 public class TodoFactory {
 
   public static Todo createTodo(String[] todoStringArray) {
     int todoId = Integer.parseInt(todoStringArray[0]);
     ZonedDateTime createdAt = ZonedDateTime.parse(todoStringArray[1]);
+
     ZonedDateTime completedAt =
-            StringUtils.isEmpty(todoStringArray[2])
+            (todoStringArray[2] == null || todoStringArray[2].isEmpty())
                     ? null
                     : ZonedDateTime.parse(todoStringArray[2]);
+
     String taskText = todoStringArray[3];
 
     Todo todo = new Todo(todoId, createdAt, completedAt, taskText);
