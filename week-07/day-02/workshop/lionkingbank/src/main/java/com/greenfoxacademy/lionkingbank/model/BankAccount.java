06df7fa8c10dcb5117ec0798d0476e28040aa52c
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-03 14:15:26 +0200

    w07d02 lionkingbank: polish

--- week-07/day-02/workshop/lionkingbank/src/main/java/com/greenfoxacademy/lionkingbank/model/BankAccount.java
+++ week-07/day-02/workshop/lionkingbank/src/main/java/com/greenfoxacademy/lionkingbank/model/BankAccount.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 @Getter
 public class BankAccount {
 
+  private final String name;
+  private final String animalType;
+  private float balance;
+
   public BankAccount(String name, String balance, String animalType) {
     this.name = name;
     this.animalType = animalType;
     this.balance = Float.parseFloat(balance);
   }
-
-  private final String name;
-  private final String animalType;
-  private float balance;
 
 }
