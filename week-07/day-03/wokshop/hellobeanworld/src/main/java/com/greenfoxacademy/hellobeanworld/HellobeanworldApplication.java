eb2fbac7e5b5c48ce6d0573c731b66bfda54d5c8
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-04 12:26:23 +0200

    w07d03 HelloBeanWorld: first steps

--- week-07/day-03/wokshop/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
+++ week-07/day-03/wokshop/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -1,13 +1,22 @@
 package com.greenfoxacademy.hellobeanworld;
 
+import com.greenfoxacademy.hellobeanworld.model.HelloWorld;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class HellobeanworldApplication {
 
+  @Autowired
+  HelloWorld helloWorld;
 
   public static void main(String[] args) {
     SpringApplication.run(HellobeanworldApplication.class, args);
+
+    AnnotationConfigApplicationContext
+
+
   }
 }
