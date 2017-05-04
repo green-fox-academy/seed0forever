7d6963f48910ab1b066f8734e168176e57206eb8
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-04 11:21:45 +0200

    w07d03 HelloBeanWorld: polish

--- week-07/day-03/wokshop/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
+++ week-07/day-03/wokshop/hellobeanworld/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellobeanworldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobeanworldApplication.class, args);
-	}
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobeanworldApplication.class, args);
+  }
 }
