2b04a3edc7e1f5ebdce00cc233134dd5948924c3
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-09 12:33:39 +0200

    w08d02 Todo-H2: implement 'Connect to database'

--- week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/controller/TodoController.java
+++ week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.todoembedded.controller;
 
+import com.greenfoxacademy.todoembedded.model.Todo;
 import com.greenfoxacademy.todoembedded.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,10 @@ public class TodoController {
 
   @RequestMapping(value = {"", "/list"})
   public String list(Model model) {
+    Todo todoTry1 = todoRepository.save(new Todo("Hello Todo World"));
+
+
+    model.addAttribute("todos", todoRepository.findAll());
     return "todolist";
   }
 }
--- week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/model/Todo.java
+++ week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/model/Todo.java
@@ -1,17 +1,38 @@
 package com.greenfoxacademy.todoembedded.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Todo {
 
+  public Todo(String title) {
+    this.title = title;
+  }
+
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
   private String title;
   private boolean isUrgent = false;
   private boolean isDone = false;
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setUrgent(boolean urgent) {
+    isUrgent = urgent;
+  }
+
+  public void setDone(boolean done) {
+    isDone = done;
+  }
 
 }
