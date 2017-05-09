56ef89975b4ec21a2ebe89078c2b8dc3d242caf5
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-09 12:37:58 +0200

    w08d02 Todo-H2: add content to database

--- week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/TodoembeddedApplication.java
+++ week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/TodoembeddedApplication.java
@@ -1,5 +1,8 @@
 package com.greenfoxacademy.todoembedded;
 
+import com.greenfoxacademy.todoembedded.model.Todo;
+import com.greenfoxacademy.todoembedded.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoembeddedApplication implements CommandLineRunner {
 
+  @Autowired
+  TodoRepository todoRepository;
+
   public static void main(String[] args) {
     SpringApplication.run(TodoembeddedApplication.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
-
+    todoRepository.save(new Todo("I have to learn Object Relational Mapping"));
+    todoRepository.save(new Todo("Start Exercise 2"));
+    todoRepository.save(new Todo("Go swimming"));
   }
 }
--- week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/controller/TodoController.java
+++ week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/controller/TodoController.java
@@ -1,6 +1,5 @@
 package com.greenfoxacademy.todoembedded.controller;
 
-import com.greenfoxacademy.todoembedded.model.Todo;
 import com.greenfoxacademy.todoembedded.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,8 +15,6 @@ public class TodoController {
 
   @RequestMapping(value = {"", "/list"})
   public String list(Model model) {
-    Todo todoTry1 = todoRepository.save(new Todo("Hello Todo World"));
-
     model.addAttribute("todos", todoRepository.findAll());
     return "todolist";
   }
