ee54b62e1ea76e792db6fc18eb3ba575c9b407a3
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-09 11:06:58 +0200

    w08d02 Todo-H2: initial commit

---
+++ week-08/day-02/workshop/todoembedded/src/main/java/com/greenfoxacademy/todoembedded/TodoembeddedApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todoembedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoembeddedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoembeddedApplication.class, args);
+	}
+}
