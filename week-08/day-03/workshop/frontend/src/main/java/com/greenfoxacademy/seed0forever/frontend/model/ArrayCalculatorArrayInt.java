6dc69da0937996bddd7804ce0246003cfcaf779a
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-10 16:28:03 +0200

    w08d03 ArrayHandler: use Lombok in both Array response classes

--- week-08/day-03/workshop/frontend/src/main/java/com/greenfoxacademy/seed0forever/frontend/model/ArrayCalculatorArrayInt.java
+++ week-08/day-03/workshop/frontend/src/main/java/com/greenfoxacademy/seed0forever/frontend/model/ArrayCalculatorArrayInt.java
@@ -1,15 +1,13 @@
 package com.greenfoxacademy.seed0forever.frontend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class ArrayCalculatorArrayInt implements RestResponseObject {
 
   private int[] result;
-
-  public ArrayCalculatorArrayInt(int[] result) {
-    this.result = result;
-  }
 }
