bc0197b259402337a65b2500df5f132845342c13
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-12 15:22:30 +0200

    w08d05 reddit: implement Post 'update' and 'delete'

--- week-08/day-05/project/reddit/src/main/java/com/greenfox/seed0forever/reddit/controller/PostController.java
+++ week-08/day-05/project/reddit/src/main/java/com/greenfox/seed0forever/reddit/controller/PostController.java
@@ -1,9 +1,13 @@
 package com.greenfox.seed0forever.reddit.controller;
 
 import com.greenfox.seed0forever.reddit.model.Post;
+import com.greenfox.seed0forever.reddit.model.PostList;
 import com.greenfox.seed0forever.reddit.repository.PostRepository;
 import java.sql.Timestamp;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +16,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class PostController {
 
   @Autowired
   PostRepository postRepository;
 
+  @Autowired
+  PostList postList;
+
   @GetMapping("/posts")
-  public Iterable<Post> getPosts() {
-    return postRepository.findAll();
+  public PostList getPosts() {
+    postList.setPosts((List<Post>) postRepository.findAll());
+    return postList;
   }
 
   @PostMapping("/posts")
@@ -27,6 +36,22 @@ public class PostController {
     receivedPost.setTimestamp(new Timestamp(System.currentTimeMillis() / 1000));
     postRepository.save(receivedPost);
     return receivedPost;
+  }
+
+  @PutMapping("/posts/{id}")
+  public Post updatePost(@RequestBody Post receivedPost, @PathVariable long id) {
+    Post currentPost = postRepository.findOne(id);
+
+    if (receivedPost.getTitle() != null && receivedPost.getTitle() != "") {
+      currentPost.setTitle(receivedPost.getTitle());
+    }
+
+    if (receivedPost.getHref() != null && receivedPost.getHref() != "") {
+      currentPost.setHref(receivedPost.getHref());
+    }
+
+    postRepository.save(currentPost);
+    return currentPost;
   }
 
   @PutMapping("/posts/{id}/upvote")
@@ -42,6 +67,13 @@ public class PostController {
     Post currentPost = postRepository.findOne(id);
     currentPost.downVote();
     postRepository.save(currentPost);
+    return currentPost;
+  }
+
+  @DeleteMapping("/posts/{id}")
+  public Post deletePost(@PathVariable long id) {
+    Post currentPost = postRepository.findOne(id);
+    postRepository.delete(currentPost);
     return currentPost;
   }
 }
