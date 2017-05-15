25a13e5c0c9fe67699f6a07d2321e7b6caf87206
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-15 11:08:54 +0200

    w09d01 groot: add test 'WithMessage'

---
+++ week-09/day-01/workshop/groot/src/test/java/com/greenfox/seed0forever/groot/controller/GuardianControllerTest.java
@@ -0,0 +1,46 @@
+package com.greenfox.seed0forever.groot.controller;
+
+import com.greenfox.seed0forever.groot.GrootApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GrootApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
+public class GuardianControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void respondGroot_WithMessage() throws Exception {
+        mockMvc.perform(get("/groot").param("message", "any message"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.received", is("any message")))
+                .andExpect(jsonPath("$.translated", is("I am Groot!")));
+    }
+
+}
