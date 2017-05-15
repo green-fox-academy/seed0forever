8898e0bdae034b2550e61d094a5199ea5f7314aa
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-05-15 16:52:04 +0200

    w09d01 groot: Rora Test: initial boilerplate code

---
+++ week-09/day-01/workshop/groot/src/test/java/com/greenfox/seed0forever/groot/controller/RoraControllerTest.java
@@ -0,0 +1,59 @@
+package com.greenfox.seed0forever.groot.controller;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.Assert.*;
+
+import com.greenfox.seed0forever.groot.GrootApplication;
+import java.nio.charset.Charset;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GrootApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
+public class RoraControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setUp() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void listCargoStatus() throws Exception {
+  }
+
+  @Test
+  public void fillShipAndShowStatus() throws Exception {
+  }
+
+}
