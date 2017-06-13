4bc5f1360ed3018380bf6c1e07b99f2ed216c9b6
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-13 16:28:07 +0200

    w12 sudokuvalidator: add two checks and another test

--- week-12/day-02/dojo/sudokuvalidator/src/main/java/SudokuValidator.java
+++ week-12/day-02/dojo/sudokuvalidator/src/main/java/SudokuValidator.java
@@ -1,6 +1,29 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class SudokuValidator {
 
   public static boolean validSolution(int[][] sudokuTable) {
+
+    for (int i = 0; i < sudokuTable.length; i++) {
+      for (int j = 0; j < sudokuTable.length; j++) {
+        if (sudokuTable[i][j] == 0) {
+          return false;
+        }
+      }
+    }
+
+    for (int i = 0; i < sudokuTable.length; i++) {
+
+      List<Integer> currentRow = IntStream.of(sudokuTable[i]).boxed().collect(Collectors.toList());
+
+      for (int digit = 1; digit <= 9; digit++) {
+        if (!currentRow.contains(digit)) {
+          return false;
+        }
+      }
+    }
 
     return true;
 
--- week-12/day-02/dojo/sudokuvalidator/src/test/java/SudokuValidatorTest.java
+++ week-12/day-02/dojo/sudokuvalidator/src/test/java/SudokuValidatorTest.java
@@ -24,14 +24,34 @@ public class SudokuValidatorTest {
           {9, 0, 1, 5, 3, 7, 2, 1, 4},
           {2, 8, 7, 4, 1, 9, 6, 3, 5},
           {3, 0, 0, 4, 8, 1, 1, 7, 9}};
+  private static final int[][] example3 = new int[][]{
+          {6, 3, 4, 6, 7, 8, 9, 1, 2},
+          {6, 7, 2, 1, 9, 5, 3, 4, 8},
+          {1, 9, 8, 3, 4, 2, 5, 6, 7},
+          {8, 5, 9, 7, 6, 1, 4, 2, 3},
+          {4, 2, 6, 8, 5, 3, 7, 9, 1},
+          {7, 1, 3, 9, 2, 4, 8, 5, 6},
+          {9, 6, 1, 5, 3, 7, 2, 8, 4},
+          {2, 8, 7, 4, 1, 9, 6, 3, 5},
+          {3, 4, 5, 2, 8, 6, 1, 7, 9}};
 
   @Before
   public void setUp() throws Exception {
   }
 
   @Test
-  public void validSolution() throws Exception {
+  public void validSolution_example1_true() throws Exception {
     assertTrue(SudokuValidator.validSolution(example1));
+  }
+
+  @Test
+  public void validSolution_example2_false() throws Exception {
+    assertFalse(SudokuValidator.validSolution(example2));
+  }
+
+  @Test
+  public void validSolution_example3_false() throws Exception {
+    assertFalse(SudokuValidator.validSolution(example3));
   }
 
 }
