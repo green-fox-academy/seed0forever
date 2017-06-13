eb91fdb9f7aa40f452588fc317343dd0f2ef9bc8
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-13 14:49:45 +0200

    w12 sudokuvalidator: add first implementation and test

---
+++ week-12/day-02/dojo/sudokuvalidator/src/main/java/SudokuValidator.java
@@ -0,0 +1,8 @@
+public class SudokuValidator {
+
+  public static boolean validSolution(int[][] sudokuTable) {
+
+    return true;
+
+  }
+}
---
+++ week-12/day-02/dojo/sudokuvalidator/src/test/java/SudokuValidatorTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class SudokuValidatorTest {
+
+  private static final int[][] example1 = new int[][]{
+          {5, 3, 4, 6, 7, 8, 9, 1, 2},
+          {6, 7, 2, 1, 9, 5, 3, 4, 8},
+          {1, 9, 8, 3, 4, 2, 5, 6, 7},
+          {8, 5, 9, 7, 6, 1, 4, 2, 3},
+          {4, 2, 6, 8, 5, 3, 7, 9, 1},
+          {7, 1, 3, 9, 2, 4, 8, 5, 6},
+          {9, 6, 1, 5, 3, 7, 2, 8, 4},
+          {2, 8, 7, 4, 1, 9, 6, 3, 5},
+          {3, 4, 5, 2, 8, 6, 1, 7, 9}};
+  private static final int[][] example2 = new int[][]{
+          {5, 3, 4, 6, 7, 8, 9, 1, 2},
+          {6, 7, 2, 1, 9, 0, 3, 4, 8},
+          {1, 0, 0, 3, 4, 2, 5, 6, 0},
+          {8, 5, 9, 7, 6, 1, 0, 2, 0},
+          {4, 2, 6, 8, 5, 3, 7, 9, 1},
+          {7, 1, 3, 9, 2, 4, 8, 5, 6},
+          {9, 0, 1, 5, 3, 7, 2, 1, 4},
+          {2, 8, 7, 4, 1, 9, 6, 3, 5},
+          {3, 0, 0, 4, 8, 1, 1, 7, 9}};
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void validSolution() throws Exception {
+    assertTrue(SudokuValidator.validSolution(example1));
+  }
+
+}
