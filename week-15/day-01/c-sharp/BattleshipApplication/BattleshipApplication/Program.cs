4e32813e34a04060725aa430d0e325d4ea1ed37f
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-03 15:42:31 +0200

    w15 c-sharp Battleship: add console input cell manipulation

--- week-15/day-01/c-sharp/BattleshipApplication/BattleshipApplication/Program.cs
+++ week-15/day-01/c-sharp/BattleshipApplication/BattleshipApplication/Program.cs
@@ -9,7 +9,21 @@ namespace BattleshipApplication
         {
             GameMap gameMap = new GameMap(20);
             GameMapFillerUtil.FillMap(gameMap);
+
+            Console.Clear();
             Console.Write(gameMap.ToString());
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                int column = -1 + int.Parse(line.Substring(0, 2));
+                int row = -1 + int.Parse(line.Substring(3, 2));
+
+                gameMap.CellMatrix[row, column] = MapCell.Miss;
+
+                Console.Clear();
+                Console.Write(gameMap.ToString());
+            }
         }
     }
 }
