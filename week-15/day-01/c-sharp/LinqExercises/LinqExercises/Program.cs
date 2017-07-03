9f2476eee3b09d977583c13051897d436d1a73c2
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-03 13:22:03 +0200

    w15 c-sharp: add LinqExercise 3

--- week-15/day-01/c-sharp/LinqExercises/LinqExercises/Program.cs
+++ week-15/day-01/c-sharp/LinqExercises/LinqExercises/Program.cs
@@ -8,15 +8,26 @@ namespace LinqExercises
     {
         public static void Main(string[] args)
         {
-            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            int[] n = {1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14};
 
             var evenNumbers = LinqExercise1(n);
+            Console.Write("Even numbers: ");
             PrintEnumerableNumbers(evenNumbers);
 
             var averageOfEvenNumbers = LinqExercise2(n);
             Console.WriteLine("Average of the even numbers: " + averageOfEvenNumbers);
 
+            IEnumerable<int> squaredPositives = LinqExercise3(n);
+            Console.Write("Square of positive numbers: ");
+            PrintEnumerableNumbers(squaredPositives);
+        }
 
+        private static IEnumerable<int> LinqExercise3(int[] numbers)
+        {
+            IEnumerable<int> squaredPositives =
+                numbers.Where(number => number > 0).Select(number => (int) Math.Pow(number, 2));
+
+            return squaredPositives;
         }
 
         private static double LinqExercise2(int[] numbers)
@@ -28,7 +39,6 @@ namespace LinqExercises
 
         private static IEnumerable<int> LinqExercise1(int[] n)
         {
-            
             IEnumerable<int> evenNumbers = n.Where(number => number % 2 == 0);
 
             return evenNumbers;
