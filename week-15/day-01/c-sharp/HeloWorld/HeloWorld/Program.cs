5b6a295fa6064179cefcd4ff39a45f712ae2b80c
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-03 11:50:59 +0200

    w15 c-sharp: add HelloWorld

---
+++ week-15/day-01/c-sharp/HeloWorld/HeloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HeloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# Rider!");
+        }
+    }
+}
