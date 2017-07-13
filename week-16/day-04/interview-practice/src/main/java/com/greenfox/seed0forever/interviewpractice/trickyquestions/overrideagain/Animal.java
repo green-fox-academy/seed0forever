2a0507baf4afe24c0e25bbdc1a2ef38d9105eea2
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-13 21:12:24 +0200

    w16d04 interview-ws: add another override tryout

---
+++ week-16/day-04/interview-practice/src/main/java/com/greenfox/seed0forever/interviewpractice/trickyquestions/overrideagain/Animal.java
@@ -0,0 +1,8 @@
+package com.greenfox.seed0forever.interviewpractice.trickyquestions.overrideagain;
+
+public class Animal {
+
+  private void sayHello() {
+    System.out.println("Animal hello!");
+  }
+}
---
+++ week-16/day-04/interview-practice/src/main/java/com/greenfox/seed0forever/interviewpractice/trickyquestions/overrideagain/Cat.java
@@ -0,0 +1,8 @@
+package com.greenfox.seed0forever.interviewpractice.trickyquestions.overrideagain;
+
+public class Cat extends Animal {
+
+  private void sayHello() {
+    System.out.println("Cat hello!");
+  }
+}
