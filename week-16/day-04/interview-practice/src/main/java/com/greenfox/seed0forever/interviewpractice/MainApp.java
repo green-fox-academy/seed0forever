691f48583df81e1eafd6ac11be57c8bea28445fa
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-13 10:24:42 +0200

    w16d04 interview-ws: try floating pont zero division

--- week-16/day-04/interview-practice/src/main/java/com/greenfox/seed0forever/interviewpractice/MainApp.java
+++ week-16/day-04/interview-practice/src/main/java/com/greenfox/seed0forever/interviewpractice/MainApp.java
@@ -8,5 +8,15 @@ public class MainApp {
   public static void main(String[] args) {
     WhatIsOverridable whatIsOverridable = new WhatIsOverridable();
     Overrider overrider = new Overrider();
+
+    executeZeroDivisionExperiment();
+  }
+
+  private static void executeZeroDivisionExperiment() {
+    System.out.println(1.0 / 0.0);
+
+    Object zeroDivisionExperiment = -1.0 / 0.0;
+    System.out.println(zeroDivisionExperiment.getClass().getTypeName());
+    System.out.println((Double) zeroDivisionExperiment * 5);
   }
 }
